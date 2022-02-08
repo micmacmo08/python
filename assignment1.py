b2790dc20a40ea3e75ed35971feb495df5f32fd3
micmacmo08 <micmacmo08@gmail.com>
2022-02-07 17:39:17 -0700

    assignment1.py: finished paymentCalculator method and implemented a small driver to enter custom values to equation

--- assignment1.py
+++ assignment1.py
@@ -1,2 +1,14 @@
-def paymentCalculator(p, r, t):
-    pass
+from math import ceil
+
+def paymentCalculator(p, r, l):
+    m = ceil(p * (r/12 * (1 + r/12)**l)/((1 + r/12)**l - 1))
+    return m
+
+def main():
+    p = int(input("What is the initial loan amount? "))
+    r = float(input("What is the annual interest rate? "))
+    r = r / 100
+    l = int(input("How many months to pay off the loan? "))
+    print(f"The monthly payment amount is ${paymentCalculator(p, r, l)}")
+
+main()
